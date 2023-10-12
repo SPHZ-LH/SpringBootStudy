4ff01365f905351a70fe91bfb3f9a1dff1c4b9aa
SPHZ_LH <1750897010@qq.com>
2023-10-13 00:13:37 +0800

    New Demo: hello world

---
+++ src/Demo1/src/main/java/com/example/demo1/Demo1Application.java
@@ -0,0 +1,15 @@
+package com.example.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//SpringBoot程序的主入口
+@SpringBootApplication
+public class Demo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1Application.class, args);
+    }
+
+}
